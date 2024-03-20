46c43204b19165ad4646eef05717698588f9bf97
Benoit Garbinato <benoit@garbinato.net>
2024-03-20 16:28:42 +0100

    Basic roundtrip with students works

---
+++ src/main/java/ch/unil/doplab/studdybuddy/StuddyBuddyService.java
@@ -0,0 +1,69 @@
+package ch.unil.doplab.studdybuddy;
+
+import ch.unil.doplab.studdybuddy.ui.StudentBean;
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.GenericType;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.List;
+
+@ApplicationScoped
+public class StuddyBuddyService {
+    private static final String BASE_URL = "http://localhost:8080/StudyBuddy-1.0-SNAPSHOT/api";
+    private WebTarget studentTarget;
+
+    @PostConstruct
+    public void init() {
+        System.err.println("StuddyBuddyService created: " + this.hashCode());
+        Client client = ClientBuilder.newClient();
+        studentTarget = client.target(BASE_URL).path("students");
+    }
+
+    public StudentBean getStudent(String id) {
+        return studentTarget
+                .path(id.toString())
+                .request()
+                .get(StudentBean.class);
+    }
+
+    public boolean setStudent(StudentBean student) {
+        var response = studentTarget
+                .path(student.getId())
+                .request(MediaType.APPLICATION_JSON)
+                .put(Entity.entity(student, MediaType.APPLICATION_JSON));
+        return response.getStatus() == 200;
+    }
+
+    public List<StudentBean> getAllStudents() {
+        return studentTarget
+                .request(MediaType.APPLICATION_JSON)
+                .get(new GenericType<List<StudentBean>>() {});
+    }
+
+    public StudentBean addStudent(StudentBean student) {
+        student.setId(null);  // To make sure the id is not set and avoid bug related to ill-formed UUID on server side
+        var response = studentTarget
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.entity(student, MediaType.APPLICATION_JSON));
+        StudentBean newlyCreatedStudent = null;
+        if (response.getStatus() == 200 && response.hasEntity()) {
+            newlyCreatedStudent = response.readEntity(StudentBean.class);
+            student.setId(newlyCreatedStudent.getId());
+        }
+        return newlyCreatedStudent;
+    }
+
+    public boolean deleteStudent(String id) {
+        var response = studentTarget
+                .path(id)
+                .request(MediaType.APPLICATION_JSON)
+                .delete();
+        return response.getStatus() == 200;
+    }
+}
---
+++ src/main/java/ch/unil/doplab/studdybuddy/ui/UserData.java
@@ -0,0 +1,45 @@
+package ch.unil.doplab.studdybuddy.ui;
+
+import ch.unil.doplab.studdybuddy.StuddyBuddyService;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@SessionScoped
+@Named
+public class UserData implements java.io.Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Inject
+    StuddyBuddyService theService;
+
+    private List<StudentBean> students;
+
+    public UserData() {
+        this.students = new ArrayList<StudentBean>();
+        System.out.println("UserData created: " + this.hashCode());
+    }
+
+    public List<StudentBean> getStudents() {
+        return students;
+    }
+
+    public void loadStudents() {
+//        students.clear();  // For local testing only
+//        populateStudents(); // For local testing only
+        students = theService.getAllStudents();
+        System.out.println("Students loaded: " + students);
+    }
+
+    // For local testing only
+    private void populateStudents() {
+        students.add(new StudentBean("b8d0c81d-e1c6-4708-bd02-d218a23e4805", "paul", "Smith", "paul.smith@gmail.com", "paul"));
+        students.add(new StudentBean("0ab2ec68-c574-4d81-bed0-a93c31fab1c0", "Jane", "Doe", "jane.doe@icloud.com", "jane"));
+        students.add(new StudentBean("5d53a98b-53a8-4580-adc1-28067b37582a", "Jean", "Dupont", "jean.dupont@facebook.com", "jean"));
+    }
+}
